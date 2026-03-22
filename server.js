ee991df05eabb3b2de36ca2dceb20daddb5df99d
Pragati Singh <pragatisingh933585@gmail.com>
2026-03-22 19:38:25 +0530

    Setup Express server and MongoDB connection

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
+require("dotenv").config()
+
 const app=require("./src/app")
+const connectToDB=require("./src/config/db")
+connectToDB();
 app.listen(3000,() => {
     console.log("Server is  runnning on port 3000")
 })
