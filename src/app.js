0229f0a17f541afc4bb45b68af965cf46befd770
Pragati Singh <pragatisingh933585@gmail.com>
2026-03-23 15:57:06 +0530

    Configured routes and middleware in app setup

--- src/app.js
+++ src/app.js
@@ -1,5 +1,10 @@
-const express =require("express")
-
-
-const app=express()
-module.exports=app
+const express = require("express");
+const cookieParser = require("cookie-parser")
+const authRouter = require("./routes/auth.routes");
+const accountRouter = require("./routes/account.routes");
+const app = express();
+app.use(express.json());
+app.use(cookieParser())
+app.use("/api/auth", authRouter);
+app.use("/api/accounts", accountRouter);
+module.exports = app;
