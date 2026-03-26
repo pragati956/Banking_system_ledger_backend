0f695420b05fe563f5a0fe2a88bbed4fdfd297aa
Pragati Singh <pragatisingh933585@gmail.com>
2026-03-26 22:26:16 +0530

    Fixed ledger model schema validation issues

--- src/models/ledger.model.js
+++ src/models/ledger.model.js
@@ -9,25 +9,27 @@ const ledgerSchema = new mongoose.Schema({
     },
     amount: {
         type: Number,
-        required: [true, "Amount is required for a ledger entry"],
-        immutable: true}
-        ,
-        transaction:{
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'Transaction',
-            required: [true, "Ledger entry must be associated with a transaction"],
-            index: true,
-            immutable: true
-        },
-        type: {
-            type: String,
-            enum: ["Credit", "Debit"],
-            message: "Type must be either Credit or Debit",
+        required: true
+    }
+    ,
+    transaction: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Transaction',
+        required: [true, "Ledger entry must be associated with a transaction"],
+        index: true,
+        immutable: true
+    },
+    type: {
+        type: String,
+        enum: {
+            values: ["Credit", "Debit"],
+            message: "Type must be either Credit or Debit"
         },
         required: [true, "Type is required for a ledger entry"],
         immutable: true
-    })
-    function preventLegderModification(){
+    }
+})
+function preventLegderModification() {
     throw new Error("Ledger entries cannot be modified once created");
 }
 ledgerSchema.pre('findOneAndUpdate', preventLegderModification);
